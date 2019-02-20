9f106bbb328e252fd82f5e09a0c584fddc3c2a5b
Sasha\Philip <philip.arvidsson97@gmail.com>
2019-02-20 01:07:14 +0100

    Added some views, some controllers and some models

--- src/CWA/Controllers/LudoController.cs
+++ src/CWA/Controllers/LudoController.cs
@@ -1,16 +1,26 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
 
-namespace CWA.Controllers
+namespace WebApplication1.Controllers
 {
     public class LudoController : Controller
     {
         public IActionResult Index()
         {
+            var client = new RestClient("http://ludogame2019.azurewebsites.net");
+
+            var request = new RestRequest("api/Ludo", Method.GET);
+
+            var ludoGameResponse = client.Execute<List<int>>(request);
+            var gameName = ludoGameResponse.Data.ToArray();
+
             return View();
         }
     }
+
 }
